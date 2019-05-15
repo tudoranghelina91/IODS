2f9f176cdd8ff5a9b2c52c9379802d2c0ab9e21f
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-05-16 02:22:48 +0300

    Implemented Stack

---
+++ IODS/DataStructures/Stack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IODS.Handlers;
+
+namespace IODS.DataStructures
+{
+    public class Stack
+    {
+        public void Push(int value)
+        {
+            StackItem newStackItem = new StackItem();
+            newStackItem.Value = value;
+            newStackItem.next = Top;
+            Top = newStackItem;
+        }
+
+        public void Pop()
+        {
+            Top = Top.next;
+        }
+
+        public void Print()
+        {
+            StackItem currentStackItem = Top;
+
+            while (currentStackItem != null)
+            {
+                Console.Write($"{currentStackItem.Value} ");
+                currentStackItem = currentStackItem.next;
+            }
+
+            Console.WriteLine();
+        }
+
+        public StackItem Top = null;
+    }
+}
---
+++ IODS/DataStructures/StackItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IODS.DataStructures
+{
+    public class StackItem
+    {
+        public int Value { get; set; }
+        public StackItem next = null;
+    }
+}
